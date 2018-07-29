9f765d73c762482f084fb7fa78169beda3c9792b
karkran <nikhil.karkra@anz.com>
2018-07-29 18:23:42 +1000

    new tnx testing

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,13 @@ const Blockchain = require('./blockchain')
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(1234, 'OINA797HJKKH', 'OINA797HJKKL');
-bitcoin.createNewBlock(1235, 'OINA797HJKKJ', 'OINA797HJKKM');
-bitcoin.createNewBlock(1236, 'OINA797HJKKK', 'OINA797HJKKN');
+bitcoin.createNewTransaction(100, 'JOHNH897JHKKJH', 'JULIAH897JHOIUOI')
+
+bitcoin.createNewBlock(123477, 'OINA797HJKLLL', 'OINA797HJKMMM');
+bitcoin.createNewTransaction(200, 'JOHNH897JHKKJG', 'JULIAH897JHOIUOE')
+bitcoin.createNewTransaction(300, 'JOHNH897JHKKJF', 'JULIAH897JHOIUOS')
+bitcoin.createNewTransaction(400, 'JOHNH897JHKKJD', 'JULIAH897JHOIUOA')
+bitcoin.createNewBlock(123477, 'OINA797HJeLLL', 'OINA797HJwMMM');
 console.log(bitcoin);
+console.log(bitcoin.chain[1]);
+console.log(bitcoin.chain[2]);
