6f67582e9f3c4972b013c50d83e4e790e33968c7
karkran <nikhil.karkra@anz.com>
2018-07-29 15:45:12 +1000

    adding last block method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -17,5 +17,8 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     this.chain.push(newBlock);
     return newBlock;
 }
+Blockchain.prototype.getLastBlock = function(){
+    return this.chain[this.chain.length-1];
+}
 
 module.exports = Blockchain
