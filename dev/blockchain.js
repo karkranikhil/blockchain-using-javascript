38dd845da2934cb6566220ed92d3afd2eeb6a0ec
karkran <nikhil.karkra@anz.com>
2018-07-29 18:06:17 +1000

    creating new transactions method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain(){
     this.chain = []; // here all block will store as a chain
-    this.newTransactions=[]; // will put the new tranactions
+    this.pendingTransactions=[]; // everytime a new trxn created it push to this.But this is not recorder into blockchain. it will record when a new block is mined/created . its an pending tnx and not validated yet. it will recorder whenwe create new block
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
@@ -8,17 +8,27 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     const newBlock = {
         index: this.chain.length+1, // describe the no of the block in the chain
         timestamp:Date.now(), // describe when this block got created
-        transactions:this.newTransactions, // all the transaction of this block ( new+ waiting)
+        transactions:this.pendingTransactions, // all the transaction of this block ( new+ waiting)
         nonce:nonce, //basically its a proof that we have created this block in a legimitate way by using proof of work method. it can be any number.
         hash:hash, // this hash will be the data of our new block. Actually we will pass our newTrnsaction in the hashing function
         previousBlockHash:previousBlockHash// this is smilar to our hash but of the previous block
     } 
-    this.newTransactions = [];
+    this.pendingTransactions = [];
     this.chain.push(newBlock);
     return newBlock;
 }
 Blockchain.prototype.getLastBlock = function(){
     return this.chain[this.chain.length-1];
+}
+
+Blockchain.prototype.createNewTransaction= function(amount, sender, recipient){
+    const newTransaction = {
+        amount:amount,
+        sender:sender,
+        recipient:recipient
+    }
+    this.pendingTransactions.push(newTransaction)
+    return this.getLastBlock()['index']+1 // return no of bloks this trnx added to
 }
 
 module.exports = Blockchain
